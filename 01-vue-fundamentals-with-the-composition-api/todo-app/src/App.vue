ed757c7d9edc61cd296dbf8150dbeaa0755d14b8
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-12 10:51:42 +0200

    feat: add checkbox

--- 01-vue-fundamentals-with-the-composition-api/todo-app/src/App.vue
+++ 01-vue-fundamentals-with-the-composition-api/todo-app/src/App.vue
@@ -8,22 +8,18 @@
     {id: 3, label: "20 cups"}
   ]);
   const newItem = ref('');
-  const newItemPriority = ref('low');
+  const newItemHighPriority = ref(false);
 </script>
 
 <template>
   <h1>{{ header }}</h1>
   <input v-model.trim="newItem" type="text" placeholder="Add a todo here..." class="todo-list-item-input"/>
-  <span>Priority</span>
+  <span class="priority-span">Priority</span>
   <label>
-    <input type="radio" v-model="newItemPriority" value="low"/> 
-    Low
+    <input type="checkbox" v-model="newItemHighPriority" />
+    High Priority
   </label>
-  <label>
-    <input type="radio" v-model="newItemPriority" value="high"/> 
-    High
-  </label>
-  {{ newItemPriority }}
+  {{ newItemHighPriority }}
   <ul>
     <li v-for="{id, label} in items" :key="id"> 
       {{ label }}
