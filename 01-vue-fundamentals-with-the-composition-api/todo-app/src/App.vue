a5a859e82cfbc4f04661a5cd745be65c2301de8f
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-12 09:44:13 +0200

    feat: add input to add new todo

--- 01-vue-fundamentals-with-the-composition-api/todo-app/src/App.vue
+++ 01-vue-fundamentals-with-the-composition-api/todo-app/src/App.vue
@@ -3,16 +3,29 @@
 
   const header = ref('ToDo App');
   const items = ref([
-    { id:1, label: 'Read'},
-    { id: 2, label: 'Rest'},
-    { id: 3, label: 'Go for a walk'}
+    {id: 1, label: "10 party hats"},
+    {id: 2, label:"2 board games"},
+    {id: 3, label: "20 cups"}
   ]);
+  const newItem = ref('');
+  const newItemPriority = ref('low');
 </script>
 
 <template>
   <h1>{{ header }}</h1>
+  <input v-model.trim="newItem" type="text" placeholder="Add a todo here..." class="todo-list-item-input"/>
+  <span>Priority</span>
+  <label>
+    <input type="radio" v-model="newItemPriority" value="low"/> 
+    Low
+  </label>
+  <label>
+    <input type="radio" v-model="newItemPriority" value="high"/> 
+    High
+  </label>
+  {{ newItemPriority }}
   <ul>
-    <li v-for="{id, label} in items" :key="id">
+    <li v-for="{id, label} in items" :key="id"> 
       {{ label }}
     </li>
   </ul>
