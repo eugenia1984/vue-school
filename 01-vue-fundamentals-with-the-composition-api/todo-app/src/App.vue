2f4cbe4c18b306861d2c7a8341cc83e75563a552
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-12 17:06:00 +0200

    feat: events

--- 01-vue-fundamentals-with-the-composition-api/todo-app/src/App.vue
+++ 01-vue-fundamentals-with-the-composition-api/todo-app/src/App.vue
@@ -3,9 +3,9 @@
 
   const header = ref('ToDo App');
   const items = ref([
-    {id: 1, label: "10 party hats"},
-    {id: 2, label:"2 board games"},
-    {id: 3, label: "20 cups"}
+    {id: 1, label: "Read"},
+    {id: 2, label:"Go out for a walk"},
+    {id: 3, label: "Dance"}
   ]);
   const newItem = ref('');
   const newItemHighPriority = ref(false);
@@ -13,13 +13,23 @@
 
 <template>
   <h1>{{ header }}</h1>
-  <input v-model.trim="newItem" type="text" placeholder="Add a todo here..." class="todo-list-item-input"/>
-  <span class="priority-span">Priority</span>
-  <label>
-    <input type="checkbox" v-model="newItemHighPriority" />
-    High Priority
-  </label>
-  {{ newItemHighPriority }}
+  <div class="flex">
+    <input v-model.trim="newItem" type="text" placeholder="Add a ToDo item here..." class="todo-list-item-input"/>
+    <span class="priority-span">Priority:</span>
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" />
+      High Priority
+    </label>
+    <br />
+    <br />
+    <button 
+      v-on:click="items.push({id: items.length + 1 ,label: newItem})" 
+      class="btn btn-primary" 
+    >
+      Save item
+    </button>
+    <br />
+  </div>
   <ul>
     <li v-for="{id, label} in items" :key="id"> 
       {{ label }}
