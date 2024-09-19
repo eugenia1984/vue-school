b18f81c3d62e2a2046df75b9802f0e06b001f896
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-19 17:40:31 +0200

    feat: add directives

--- 02-vue-fundamentals-api-composition/src/App.vue
+++ 02-vue-fundamentals-api-composition/src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <h1>Hi {{ name.toUpperCase() }}</h1>
+  <hr />
   <h2 :style="colorBlue">I'm blue</h2>
-  <h3>{{ isActive ? 'I am active' : 'I am inactive' }}</h3>
+  <hr/>
+  <p>{{ isActive ? 'I am active' : 'I am inactive' }}</p>
+  <hr />
+  <div>
+    <h2>v-if & v-else</h2>
+    <p v-if="isOlder">I'm older</p>
+    <p v-else>I'm not older</p>
+  </div>
+  <hr/>
+  <div>
+    <h2>v-if, v-else-if & v-else</h2>
+    <p v-if="iAm18 > 18">I'm older than 18.</p>
+    <p v-else-if="iAm18 === 18">I'm 18.</p>
+    <p v-else>I'm younger than 18.</p>
+  </div>
 </template>
 
 <script setup>
 const name = "Eugenia";
 const colorBlue = "color: blue";
-const isActive = true;
+const isActive = true; 
+const isOlder = false;
+const iAm18 = 18;
 </script>
 
 <style>
